147b7e7ea29edad9c6c31431c94b70cbac7a4511
caglorithm <caglorithm@users.noreply.github.com>
2020-04-09 23:40:16 +0000

    deploy: 1e756873a5bc63e42ee0f6a9a834ab55ff25057c

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/covid19-analysis/assets/js/search-data.json', true);
+    request.open('GET', '/notebooks/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
