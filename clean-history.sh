fd0d06b439a50144d54a27d405436daedc758df9
caglorithm <caglorithm@users.noreply.github.com>
2020-10-30 08:37:41 +0000

    deploy: 35f1d08aeb0467f8967fd6975f84f1de780d0772

---
+++ clean-history.sh
@@ -0,0 +1,11 @@
+-- Remove the history from 
+rm -rf .git
+
+-- recreate the repos from the current content only
+git init
+git add .
+git commit -m "Clean history"
+
+-- push to the github remote repos ensuring you overwrite history
+git remote add origin git@github.com:caglorithm/notebooks.git
+git push -u --force origin master
